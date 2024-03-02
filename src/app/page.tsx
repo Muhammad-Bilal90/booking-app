452ee39447362f1bb12e884f7699e7d81cee35b7
Muhammad Bilal <61741931+Muhammad-Bilal90@users.noreply.github.com>
2024-03-02 16:05:21 +0500

    home page for displaying all hotels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import HotelList from "@/components/hotel/HotelList";
+import { getHotels } from "../../actions/getHotels";
+
+interface HomeProps {
+  searchParams: {
+    title: string;
+    country: string;
+    state: string;
+    city: string;
+  };
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const hotels = await getHotels(searchParams);
+
+  if (!hotels) return <div>No hotel found...</div>;
+
+  return (
+    <div>
+      <HotelList hotels={hotels} />
+    </div>
+  );
 }
